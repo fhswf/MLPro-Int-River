5abb55ee7ef8013cca881424586c6cd933f93dea
detlefarend <detlef@arend-privat.net>
2024-01-31 10:31:09 +0100

    #

--- src/setup.py
+++ src/setup.py
@@ -12,18 +12,11 @@ packages=['mlpro_int_river'],
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "dill==0.3.6",
-        "numpy==1.23.5",
-        "matplotlib==3.6.3",
-        "transformations==2022.9.26",
-        "scipy==1.8.1",
-        "pymunk==6.4.0",
-        "multiprocess==0.70.14",
-        "scikit-learn==1.2.0",
-        "optuna==3.0.5",
-        "hyperopt==0.2.7",
-        "pyglet==1.5.27",
-        "mlpro>=2.0.0"
+        "dill",
+        "numpy",
+        "matplotlib",
+        "multiprocess",
+        "mlpro>=1.2.0",
         "river==0.14.0"
     ],
 },
