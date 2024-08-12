9c2d53a85e9f2dcf9f868cbb3715559b59114c09
detlefarend <detlef@arend-privat.net>
2024-08-12 14:56:04 +0200

    Preparation of v0.2.0

--- doc/rtd/conf.py
+++ doc/rtd/conf.py
@@ -22,7 +22,7 @@ copyright = '2024 South Westphalia University of Applied Sciences, Germany'
 author = 'Detlef Arend, Steve Yuwono, Laxmikant Shrikant Baheti et al'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.6'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
